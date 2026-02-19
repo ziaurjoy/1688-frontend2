5ba782ebaec89f549d44b8aa8ea705d835deef2b
joypythondev <ATBBNq2rv3Ugrv5tMc77dbP6QQS2CCE2C891>
2026-02-19 16:14:19 +0600

    remove cache

--- src/types/set-state-action-type.ts
+++
@@ -1,3 +0,0 @@
-import type { Dispatch, SetStateAction } from "react";
-
-export type SetStateActionType<T> = Dispatch<SetStateAction<T>>;
--- src/utils/timeframe-extractor.ts
+++
@@ -1,9 +0,0 @@
-export function createTimeFrameExtractor(
-  selectedTimeFrame: string | undefined,
-) {
-  return (sectionKey: string) => {
-    return selectedTimeFrame
-      ?.split(",")
-      .find((value) => value.includes(sectionKey));
-  };
-}
